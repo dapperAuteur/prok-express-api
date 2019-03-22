e848cbbb34ec9558692c1e9390f0e8aaff13e18f
dapperauteur <a@awews.com>
2019-03-21 20:25:16 -0700

    add middleware to ensure users are authenticated and authorized to perform destructive actions

---
+++ middleware/middleware_auth.js
@@ -0,0 +1,36 @@
+exports.loginRequired = function(req, res, next) {
+  const user = req.session.user;
+  if (user._id) {
+    next();
+  } else {
+    res.status(401).json({ message: "Please log in first" });
+  }
+  res.status(401).json({
+    message: "There was a problem with your request. Are you logged in?"
+  });
+};
+
+exports.ensureCorrectUserRole = function(req, res, next) {
+  const user = req.session.user;
+  const userRole = parseInt(user.userRole);
+  if (userRole < 3) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "You do NOT have the proper credentials for this action."
+    });
+  }
+};
+
+exports.ensureCorrectUser = function(req, res, next) {
+  const user = req.session.user;
+  if (user._id === req.body.user._id) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "You are NOT allowed to access another player's content."
+    });
+  }
+};
+
+module.exports = exports;
