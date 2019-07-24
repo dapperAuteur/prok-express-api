038becec23b0000135d46f645849bd3204a7fd0c
Anthony McDonald <a@awews.com>
2019-07-23 17:05:47 -0700

    remove socket.io code and routes to prepare for graphql

--- routes/routes_auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const helpersAuth = require("./../helpers/helpers_auth");
-
-router.post("/sign-in", helpersAuth.signin);
-router.post("/sign-up", helpersAuth.signup);
-router.post("/sign-out", helpersAuth.signout);
-
-module.exports = router;
